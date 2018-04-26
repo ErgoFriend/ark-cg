7907ce06d8e85ae30c5af1e1910b94f93e9f7499
Kensei Yaegashi <s1F101702857@iniad.org>
2018-04-26 20:21:52 +0900

    hpの更新

--- home/views.py
+++ home/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 from django.http.response import HttpResponse
- 
+
 def index(request):
-    return render(request, 'home/index.html')
+    return render(request, 'home/circle_hp.html')
