7f5bb0a1404015f0dd6bd07ccaacf044a55e4911
Kaito Isobe <s1f101703724@iniad.org>
2018-04-27 20:58:46 +0900

    path

--- home/urls.py
+++ home/urls.py
@@ -3,6 +3,6 @@ from . import views
 app_name='home'
 urlpatterns = [
     re_path(r'^$', views.index, name='index'),
-    re_path(r'^contact/$', views.contact, name="contact"),
-    re_path(r'^works/$', views.works, name = 'works')
+    path('contact', views.contact),
+    path('works', views.works)
 ]
