ee8ee80a8900ebb5feedc84fb133293659824e79
Kaito Isobe <s1f101703724@iniad.org>
2018-04-27 20:52:21 +0900

    urls

--- home/urls.py
+++ home/urls.py
@@ -2,7 +2,7 @@ from django.urls import path,re_path
 from . import views
 app_name='home'
 urlpatterns = [
-    re_path(r'^', views.index, name='index'),
-    re_path(r'^contact/', views.contact, name="contact"),
-    re_path(r'^works/', views.works, name = 'works')
+    re_path(r'^$', views.index, name='index'),
+    re_path(r'^contact/$', views.contact, name="contact"),
+    re_path(r'^works/$', views.works, name = 'works')
 ]
