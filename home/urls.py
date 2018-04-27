b5197c5014f9e089babcbc9738d5e37479327c6e
Kensei Yaegashi <s1F101702857@iniad.org>
2018-04-27 19:36:13 +0900

    urlの追加

--- home/urls.py
+++ home/urls.py
@@ -3,4 +3,6 @@ from . import views
 app_name='home'
 urlpatterns = [
     re_path(r'^', views.index, name='index'),
+    re_path(r'^ark/contact', views.circle_contact, name="contact"),
+    re_path(r'^ark/works', views.circle_works, name = 'works')
 ]
--- home/views.py
+++ home/views.py
@@ -3,3 +3,9 @@ from django.http.response import HttpResponse
 
 def index(request):
     return render(request, 'home/circle_hp.html')
+
+def works(request):
+    return render(request, 'home/circle_works.html')
+
+def contact(request):
+    return render(request, 'home/circle_contact.html')
