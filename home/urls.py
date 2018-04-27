a84359f84db49e1727ce21207898c64714d448cd
Kaito Isobe <s1f101703724@iniad.org>
2018-04-27 20:32:15 +0900

    renew
    asa

--- home/urls.py
+++ home/urls.py
@@ -3,6 +3,6 @@ from . import views
 app_name='home'
 urlpatterns = [
     re_path(r'^', views.index, name='index'),
-    re_path(r'^ark/contact', views.circle_contact, name="contact"),
-    re_path(r'^ark/works', views.circle_works, name = 'works')
+    re_path(r'^contact', views.contact, name="contact"),
+    re_path(r'^works', views.works, name = 'works')
 ]
