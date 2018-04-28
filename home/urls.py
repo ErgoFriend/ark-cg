28b98d883ef24ea75b9ba4c14c2c29fb80a58aac
Kensei Yaegashi <s1F101702857@iniad.org>
2018-04-28 09:09:17 +0900

    変更

--- home/urls.py
+++ home/urls.py
@@ -2,7 +2,7 @@ from django.urls import path,re_path
 from . import views
 app_name='home'
 urlpatterns = [
-    re_path(r'^', views.index, name='index'),
-    re_path(r'^ark/contact', views.circle_contact, name="contact"),
-    re_path(r'^ark/works', views.circle_works, name = 'works')
+    path('', views.index, name='index'),
+    path('contact', views.contact,name='contact'),
+    path('works', views.works,name='works')
 ]
