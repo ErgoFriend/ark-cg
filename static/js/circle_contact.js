1ba1314cd9c85aa7ef57ecb7a5837cf51c1e49ed
EC2 Default User <ec2-user@ip-172-31-35-68.ap-northeast-1.compute.internal>
2018-04-29 10:46:29 +0900

    google analy

---
+++ static/js/circle_contact.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+  $(".drawer").drawer();
+});
+
+$(function() {
+	// 初期位置へ移動
+	$('.navi span').css({
+		width: $('.navi .current').outerWidth(),
+		left: $('.navi .current').position().left
+	});
+	// マウスオーバーでアニメーション
+	$('.navi a').mouseover(function(){
+		$('.navi span').stop().animate({
+			width: $(this).outerWidth(),
+			left: $(this).position().left}
+		,'fast');
+	});
+});
