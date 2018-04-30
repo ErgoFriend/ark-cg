c283720243ab12ed81ead71df1d1e699001c1132
Kensei Yaegashi <s1F101702857@iniad.org>
2018-05-01 02:05:11 +0900

    html urls

--- home/urls.py
+++ home/urls.py
@@ -4,6 +4,6 @@ app_name='home'
 urlpatterns = [
     path('', views.index, name='index'),
     path('contact', views.contact,name='contact'),
-    path('works', views.works,name='works')
+    path('works', views.works,name='works'),
     path('about_us', views.about_us,name='about_us')
 ]
