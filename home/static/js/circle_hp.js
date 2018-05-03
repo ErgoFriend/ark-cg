3b8f7c88a29e34ffb4d06ea284e530718b19e84c
Kensei Yaegashi <s1F101702857@iniad.org>
2018-05-04 01:44:39 +0900

    js

--- home/static/js/circle_hp.js
+++ home/static/js/circle_hp.js
@@ -1,3 +1,13 @@
+$(function(){
+
+    // サイトアクセス時に非表示にしてから、フェードインさせる
+    $('.btn').hide();
+    $('.btn').fadeIn(2000);
+
+    // リンククリック時にフェードアウトしてから、画面遷移する
+
+});
+
 
 $(document).ready(function() {
   $(".drawer").drawer();
@@ -12,7 +22,19 @@ $(function() {
 	// マウスオーバーでアニメーション
 	$('.navi a').mouseover(function(){
 		$('.navi span').stop().animate({
-			width: $(this).outerWidth(),
+      $(function(){
+
+          // サイトアクセス時に非表示にしてから、フェードインさせる
+          $('#icon').hide();
+          $('#icon').fadeIn(2000);
+          $('.google-map').hide();
+          $('.google-map').fadeIn(2000);
+
+          // リンククリック時にフェードアウトしてから、画面遷移する
+
+      });width: $(this).outerWidt
+
+      h(),
 			left: $(this).position().left}
 		,'fast');
 	});
