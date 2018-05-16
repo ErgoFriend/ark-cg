8578ba79749dccf97940e6e91a4361f36a38d2e4
ErgoFriend <ergoproxy2236@yahoo.co.jp>
2018-05-16 15:40:07 +0900

    one

--- home/models.py
+++ home/models.py
@@ -3,22 +3,25 @@ from django.utils import timezone
 
 # Create your models here.
 
-class Gallery(models.Model):
-    """作品のモデル"""
-    title = models.CharField(max_length=256)
-    content = models.TextField()
-    date = models.DateTimeField('date published', default=timezone.now)
-    category = models.ForeignKey(Category)
-
 class Category(models.Model):
     """カテゴリー
     Galary(CG,イラスト,サービス)
     News,Blog"""
     name = models.CharField(max_length=256)
 
+class Gallery(models.Model):
+    """作品のモデル"""
+    title = models.CharField(max_length=256)
+    content = models.TextField()
+    date = models.DateTimeField('date published', default=timezone.now)
+    category = models.ForeignKey(Category,on_delete=models.CASCADE)
+
 class News(models.Model):
     """アクティビティ"""
     title = models.CharField(max_length=256)
     content = models.TextField()
     date = models.DateTimeField('date published', default=timezone.now)
-    category = models.ForeignKey(Category)
+    category = models.ForeignKey(Category,on_delete=models.CASCADE)
+
+class Image(models.Model):
+    image = models.ImageField(upload_to='images/')
